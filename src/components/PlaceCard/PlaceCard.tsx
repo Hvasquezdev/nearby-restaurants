4ff261eed10360bb3cbbae945b322e95e647e72e
Hector Vazquez <hvasquezdev@gmail.com>
2023-04-14 17:09:23 -0300

    feat: add PlaceCard component

---
+++ src/components/PlaceCard/PlaceCard.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import starImage from "./../../assets/favorite.svg";
+import "./PlaceCard.scss";
+
+interface PlaceCardProps {
+  iconUrl: string;
+  direction: string;
+  name: string;
+  rating: number;
+  placeId: string;
+}
+
+const PlaceCard = (props: PlaceCardProps) => {
+  return (
+    <div className="place-card">
+      <div className="place-card__icon">
+        <img src={props.iconUrl} alt="Place icon" width={24} height={24} />
+      </div>
+
+      <div className="place-card__content">
+        <div className="place-details">
+          <h3 className="place-details__name">{props.name}</h3>
+          <p className="place-details__direction">{props.direction}</p>
+        </div>
+
+        <div className="place-rating">
+          Rating: {props.rating}
+          <img
+            className="star-icon"
+            src={starImage}
+            alt="Rating star"
+            width={16}
+            height={16}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlaceCard;
