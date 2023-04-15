0df99aebd98fa0a1a1e549deab512000263bac6d
Hector Vazquez <hvasquezdev@gmail.com>
2023-04-15 11:59:06 -0300

    feat(PlacesWrapper): add empty state and use redux to get location

--- src/components/PlacesWrapper/PlacesWrapper.tsx
+++ src/components/PlacesWrapper/PlacesWrapper.tsx
@@ -1,28 +1,42 @@
-import { placeAutocompleteMock, placesMock } from "../../mocks/placesMock";
 import { usePlaces } from "../../hooks/usePlaces";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 import PlaceCard from "../PlaceCard/PlaceCard";
+import EmptyState from "../EmptyState/EmptyState";
 import "./PlacesWrapper.scss";
 
 const PlacesWrapper = () => {
-  const { placeResults } = usePlaces({
-    location: {
-      lat: -34.5075009,
-      lng: -58.4794951,
-    },
+  const locationLatLng = useSelector(
+    ({ location }: RootState) => location.locationLatLng
+  );
+  const { placeResults, isLoading } = usePlaces({
+    location: locationLatLng,
     type: "restaurant",
   });
 
   return (
     <div className="places-wrapper">
-      {placeResults.map((place) => (
-        <PlaceCard
-          iconUrl={place.icon || ""}
-          name={place.name || "N/A"}
-          direction={place.vicinity || "N/A"}
-          rating={place.rating || 0}
-          placeId={place.place_id || ""}
+      {isLoading || !placeResults.length ? (
+        <EmptyState
+          description={
+            isLoading
+              ? "Loading restaurants nearby"
+              : "Search a location to see nearby restaurants"
+          }
         />
-      ))}
+      ) : null}
+
+      {!isLoading &&
+        placeResults.map((place, index) => (
+          <PlaceCard
+            iconUrl={place.icon || ""}
+            name={place.name || "N/A"}
+            direction={place.vicinity || "N/A"}
+            rating={place.rating || 0}
+            placeId={place.place_id || ""}
+            key={`${index}-${place.place_id}`}
+          />
+        ))}
     </div>
   );
 };
